3e5ed8a2e49548b24e0ec2eed7021bac4581b91e
Mori Daisuke <troussierd@gmail.com>
2018-12-04 16:23:17 +0900

    no message

--- a.js
+++ a.js
@@ -1,1 +1,7 @@
-console.log($('.p-list__item'));
+if ((location.href).match(/\/car\//)) {
+    if ((location.href).match(/_page2_/)) {
+        $('[name="loan_amount"]').val('100');
+        $('[name="loan_bonus"]').val('50').trigger('change');
+        $('[name="loan_date[Y]"] option[value="30"]').prop('selected', true);
+    }
+}
